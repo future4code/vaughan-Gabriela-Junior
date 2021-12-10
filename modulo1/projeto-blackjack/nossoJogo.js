4d9731bbc49e8b8f8d52625f8653b42595d61ba1
Gabriela Hermenegildo <gabriela.hermenegildo@gmail.com>
2021-12-10 16:50:23 -0300

    projeto-blackjack-atualizado

--- modulo1/projeto-blackjack/nossoJogo.js
+++ modulo1/projeto-blackjack/nossoJogo.js
@@ -15,20 +15,20 @@ console.log(`Boas vindas ao jogo de Blackjack!`);
 
 if(confirm(`Quer iniciar uma nova rodada?`)) {
 
-const carta1 = comprarCarta();
+const cartaUsuario1 = comprarCarta();
 
-const carta2 = comprarCarta();
+const cartaUsuario2 = comprarCarta();
 
-const carta3 = comprarCarta();
+const cartaComputador1 = comprarCarta();
 
-const carta4 = comprarCarta();
+const cartaComputador2 = comprarCarta();
 
-const somaValorCartaUsuario = carta1.valor + carta2.valor;
+const somaValorCartaUsuario = cartaUsuario1.valor + cartaUsuario2.valor;
 
-const somaValorCartaComputador = carta3.valor + carta4.valor;
+const somaValorCartaComputador = cartaComputador1.valor + cartaComputador2.valor;
 
-console.log(`Usuário - cartas: ${carta1.texto} ${carta2.texto} - pontuação ${somaValorCartaUsuario}`);
-console.log(`Computador - cartas: ${carta3.texto} ${carta4.texto} - pontuação ${somaValorCartaComputador}`)
+console.log(`Usuário - cartas: ${cartaUsuario1.texto} ${cartaUsuario2.texto} - pontuação ${somaValorCartaUsuario}`);
+console.log(`Computador - cartas: ${cartaComputador1.texto} ${cartaComputador2.texto} - pontuação ${somaValorCartaComputador}`)
 
 function retornarMensagens (somaUsuario, somaComputador) {
     if (somaUsuario === somaComputador) {
