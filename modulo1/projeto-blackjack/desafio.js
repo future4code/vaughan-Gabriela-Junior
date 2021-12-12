f24f7be330a60a0e58f17bb616136b566632ca3f
Gabriela Hermenegildo <gabriela.hermenegildo@gmail.com>
2021-12-12 18:09:11 -0300

    feito-o-desafio-do-projeto

--- modulo1/projeto-blackjack/desafio.js
+++ modulo1/projeto-blackjack/desafio.js
@@ -12,21 +12,25 @@
  */
 
 
-if(confirm(`Boas vindas ao jogo de Blackjack!\nQuer iniciar uma nova rodada?`)) {
-   
-let cartaUsuario1 = comprarCarta();
+if (confirm(`Boas vindas ao jogo de Blackjack!\nQuer iniciar uma nova rodada?`)) {
 
-let cartaUsuario2 = comprarCarta();
+   let cartaUsuario1 = comprarCarta();
 
-let cartaComputador1 = comprarCarta();
+   let cartaUsuario2 = comprarCarta();
 
-let cartaComputador2 = comprarCarta();
+   let cartaComputador1 = comprarCarta();
 
-let arrayCartasUsuario = [cartaUsuario1, cartaUsuario2];
+   let cartaComputador2 = comprarCarta();
 
-let arrayCartasComputador = [cartaComputador1, cartaComputador2];
+   let arrayCartasUsuario = [cartaUsuario1, cartaUsuario2];
 
-   if (cartaUsuario1.valor === 11 && cartaUsuario2.valor === 11){
+   let arrayCartasComputador = [cartaComputador1, cartaComputador2];
+
+   let somaCartasUsuario = 0;
+   let somaCartasComputador = 0;
+
+
+   if (cartaUsuario1.valor === 11 && cartaUsuario2.valor === 11) {
       cartaUsuario1 = comprarCarta();
       cartaUsuario2 = comprarCarta();
 
@@ -35,35 +39,48 @@ let arrayCartasComputador = [cartaComputador1, cartaComputador2];
       cartaComputador2 = comprarCarta();
    }
 
-function retornaPrimeiraRodada (carta1, carta2, carta3, carta4, carta5, carta6) {
+   for (let i = 0; i < arrayCartasUsuario.length; i++) {
+      somaCartasUsuario += arrayCartasUsuario[i].valor
+   }
 
-   const somaCartasUsuario = carta1.valor + carta2.valor;
-   const somaCartasComputador = carta3.valor + carta4.valor
-   
-      if(confirm(`Suas cartas são ${carta1.texto} ${carta2.texto}. A carta revelada do computador é ${carta3.texto}\nDeseja comprar mais uma carta?`)) {
-            (confirm(`Suas cartas são ${carta1.texto} ${carta2.texto} ${carta5.texto}. A carta revelada do computador é ${carta3.texto}\nDeseja comprar mais uma carta?`))
-               
-      }else{
-         if (somaCartasUsuario === somaCartasComputador) {
-            alert(`Suas cartas são ${carta1.texto} ${carta2.texto}. 
-            Sua pontuação é ${somaCartasUsuario}\nAs cartas do computador são ${carta3.texto} ${carta4.texto}. A pontuação do computador é ${somaCartasComputador}\nEmpate!`)
-         } else if (somaCartasComputador > 21) {
-            alert(`Suas cartas são ${carta1.texto} ${carta2.texto}. 
-            Sua pontuação é ${somaCartasUsuario}\nAs cartas do computador são ${carta3.texto} ${carta4.texto}. A pontuação do computador é ${somaCartasComputador}\nO usuário ganhou!`)
-         } else if (somaCartasUsuario > 21) {
-            alert(`Suas cartas são ${carta1.texto} ${carta2.texto}. 
-            Sua pontuação é ${somaCartasUsuario}\nAs cartas do computador são ${carta3.texto} ${carta4.texto}. A pontuação do computador é ${somaCartasComputador}\nO computador ganhou!`)
-         } else if (somaCartasUsuario > somaCartasComputador) {
-            alert(`Suas cartas são ${carta1.texto} ${carta2.texto}. 
-            Sua pontuação é ${somaCartasUsuario}\nAs cartas do computador são ${carta3.texto} ${carta4.texto}. A pontuação do computador é ${somaCartasComputador}\nO usuário ganhou!`)
-         } else if (somaCartasUsuario < somaCartasComputador) {
-            alert(`Suas cartas são ${carta1.texto} ${carta2.texto}. 
-            Sua pontuação é ${somaCartasUsuario}\nAs cartas do computador são ${carta3.texto} ${carta4.texto}. A pontuação do computador é ${somaCartasComputador}\nO computador ganhou!`)
-            }
+   const perguntarUsuarioLoop = (array) => {
+      let comprarCartaUsuario = confirm(`Suas cartas são ${arrayCartasUsuario.map(carta => carta.texto)}. Seu total de pontos é: ${somaCartasUsuario}. A carta revelada do computador é ${cartaComputador1.texto}.
+      \nDeseja comprar mais uma carta?`)
+      while (comprarCartaUsuario) {
+         let novaCarta = comprarCarta()
+         array.push(novaCarta)
+         somaCartasUsuario += novaCarta.valor
+         if (somaCartasUsuario > 21) {
+            comprarCartaUsuario = false
+            alert(`Você perdeu!`)
+         } else {
+            comprarCartaUsuario = confirm(`Suas cartas são ${arrayCartasUsuario.map(carta => carta.texto)}. Seu total de pontos é: ${somaCartasUsuario}. A carta revelada do computador é ${cartaComputador1.texto}.
+         \nDeseja comprar mais uma carta?`)
          }
       }
-   
-   retornaPrimeiraRodada(cartaUsuario1, cartaUsuario2, cartaComputador1, cartaComputador2);
+   }
+
+   perguntarUsuarioLoop(arrayCartasUsuario);
+
+   arrayCartasComputador.map(carta => somaCartasComputador += carta.valor);
+
+
+   while (somaCartasComputador < 20 && somaCartasComputador < somaCartasUsuario && somaCartasUsuario <= 21) {
+      let novaCarta = comprarCarta()
+      arrayCartasComputador.push(novaCarta)
+      somaCartasComputador += novaCarta.valor
+   }
+
+   console.log(arrayCartasComputador);
+   console.log(somaCartasComputador);
+
+   if ((somaCartasUsuario > 21 || somaCartasUsuario < somaCartasComputador) && somaCartasComputador <= 21) {
+      alert(`Você perdeu! Seu total de pontos foi: ${somaCartasUsuario}.\nO total do computador foi: ${somaCartasComputador}`)
+   } else if (somaCartasUsuario === somaCartasComputador) {
+      alert(`Vocês empataram! Seu total de pontos e do computador foi: ${somaCartasUsuario}`)
+   } else {
+      alert(`Você ganhou, parabéns! Seu total de pontos foi: ${somaCartasUsuario}.\nO total do computador foi: ${somaCartasComputador}`)
+   };
 
 } else {
    alert(`O jogo acabou!`);
