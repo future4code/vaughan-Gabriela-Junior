ab03917290fdd14d092a0ab0d024b876c3c4262e
Gabriela Hermenegildo <gabriela.hermenegildo@gmail.com>
2022-01-16 18:20:03 -0300

    arrumadas as caixas de texto para quebrar o texto

--- modulo2/projeto-whatslab/whatslab/src/components/SecaoMensagem/style-SecaoMensagem.js
+++ modulo2/projeto-whatslab/whatslab/src/components/SecaoMensagem/style-SecaoMensagem.js
@@ -21,17 +21,23 @@ export const BalaoMensagem = styled.div`
     margin: 10px;
     border-radius: 0.5em;
     box-shadow: 2px 3px 5px 2px #121111;
+    max-width: 50%;
+    word-wrap: break-word;
     `
 
 export const BalaoMensagemEu = styled.div`
     display: flex;
+    display: inline-block;
+    word-wrap: break-word;
     padding: 10px;
     background-color: #b88ad1;
     align-self: flex-end;
+    text-align: left;
     color: black;
     margin: 10px;
     border-radius: 0.5em;
     box-shadow: 2px 3px 5px 2px #121111;
+    max-width: 50%;
 `
 
 export const InputContainer = styled.div`
