bd292c883d0dacd090153ac67c07501ea6044a7a
Gabriela Hermenegildo <gabriela.hermenegildo@gmail.com>
2022-01-25 18:48:45 -0300

    feito exercicio de integração de apis

---
+++ modulo2/apis-labenusers/labenusers/src/App.js
@@ -0,0 +1,162 @@
+import React from 'react';
+import axios from 'axios';
+import { createGlobalStyle } from 'styled-components';
+import { MainStyle, RegistrationContainer, Input, ButtonListUsers, ButtonCreateUser, ListUsersContainers, ButtonReturn, ListOfUsersStyle } from './style-app.js';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+  }
+
+  body {
+    background-color: #171717;
+    color: white;
+  }
+`
+
+class App extends React.Component {
+
+  state = {
+    users: [],
+    inputName: "",
+    inputEmail: "",
+    userList: false,
+  }
+
+  componentDidMount() {
+    this.getAllUsers()
+  }
+
+  addName = (event) => {
+    this.setState({ inputName: event.target.value })
+  };
+
+  addEmail = (event) => {
+    this.setState({ inputEmail: event.target.value })
+  };
+
+  createUser = () => {
+    const url = "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users"
+    const body = {
+      name: this.state.inputName,
+      email: this.state.inputEmail,
+    }
+    const config = {
+      headers: {
+        Authorization: "gabriela-junior-vaughan"
+      }
+    }
+
+    axios.post(url, body, config)
+      .then((response) => {
+        console.log(JSON.stringify(response.data));
+        this.setState({ inputName: "", inputEmail: "" })
+        alert(`Usuário criado com sucesso`)
+        this.getAllUsers()
+      })
+      .catch((error) => {
+        console.log(error.response.data);
+        alert(`Erro na criação do usuário: ${error.response.data.message}`)
+      });
+  };
+
+  getAllUsers = () => {
+    const url = 'https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users'
+    const config = {
+      headers: {
+        Authorization: "gabriela-junior-vaughan"
+      }
+    };
+
+    axios.get(url, config)
+      .then((response) => {
+        this.setState({ users: response.data })
+        console.log(response.data)
+      })
+      .catch((error) => {
+        console.log(error.response.data)
+      })
+  };
+
+  deleteUser = (userId) => {
+    const url = `https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users/${userId}`
+    const config = {
+      headers: {
+        Authorization: "gabriela-junior-vaughan"
+      }
+    }
+
+    axios.delete(url, config)
+      .then((response) => {
+        console.log(response.data)
+        alert(`Usuário deletado com sucesso!`)
+        this.getAllUsers()
+      })
+      .catch((error) => {
+        console.log(error.response.data)
+        alert(`Erro ao deletar usuário: ${error.response.data.message}`)
+      })
+  }
+
+  changeRenderization = () => {
+    this.setState({
+      userList: !this.state.userList
+    })
+  }
+
+  render() {
+
+    const usersList = this.state.users.map((user) => {
+      return (
+        <ListOfUsersStyle key={user.id}>
+          {user.name} <button onClick={() => { this.deleteUser(user.id) }}>X</button>
+        </ListOfUsersStyle>
+      )
+    });
+
+    const renderListOfUsers = () => {
+      if (this.state.userList) {
+        return (
+          <ListUsersContainers>
+            <h1>Lista de usuários</h1>
+              {usersList}
+              <ButtonReturn onClick={this.changeRenderization}>Voltar</ButtonReturn>
+          </ListUsersContainers>
+        )
+      } else {
+        return (
+          <RegistrationContainer>
+            <h1>Cadastro</h1>
+            <Input
+              placeholder="Nome do Usuário"
+              value={this.state.inputName}
+              onChange={this.addName}
+            />
+            <Input
+              placeholder="Email do Usuário"
+              value={this.state.inputEmail}
+              onChange={this.addEmail}
+            />
+            <div>
+              <ButtonCreateUser onClick={this.createUser}>Criar Usuário</ButtonCreateUser>
+            </div>
+          </RegistrationContainer>
+        )
+      }
+    }
+
+    return (
+      <MainStyle>
+        <GlobalStyle />
+        <div>
+          {this.state.userList === false && <ButtonListUsers onClick={this.changeRenderization}> Ver Lista de Usuários</ButtonListUsers>}
+        </div>
+        {renderListOfUsers()}
+
+      </MainStyle>
+    );
+  }
+}
+
+export default App;
---
+++ modulo2/apis-labenusers/labenusers/src/style-app.js
@@ -0,0 +1,110 @@
+import styled from 'styled-components';
+
+export const MainStyle = styled.div`
+    display: grid;
+    justify-content: center;
+    align-items: center;
+    justify-items: center;
+    align-content: center;
+    width: 100vw;
+    /* height: 70vh; */
+`
+
+// Estilização Área de Cadastro
+export const RegistrationContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    border: 1px solid white;
+    width: 100%;
+    padding: 20px;
+    margin: 20px;
+`
+
+export const Input = styled.input`
+    margin: 20px 20px 10px;
+    padding: 10px;
+`
+
+export const ButtonListUsers = styled.button`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 30px;
+    padding: 10px;
+    background-color: black;
+    color: white;
+    border: 1px solid white;
+    cursor: pointer;
+
+    :hover {
+    background-color: lightgray;
+    color: black;
+    }
+`
+
+export const ButtonCreateUser = styled.button`
+    padding: 10px;
+    margin-top: 10px;
+    background-color: black;
+    color: white;
+    border: 1px solid white;
+    cursor: pointer;
+
+    :hover {
+    background-color: lightgray;
+    color: black;
+    }
+`
+
+//Estilização Lista de Usuários
+
+export const ListUsersContainers = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    border: 1px solid white;
+    width: 100%;
+    padding: 20px;
+    margin: 20px 0;
+    h1{
+        margin-bottom: 10px;
+    }
+`
+
+export const ListOfUsersStyle = styled.div`
+    margin: 10px;
+    padding: 10px;
+    border: 1px solid white;
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    button{
+        border: 1px solid white;
+        padding: 0 10px;
+        background-color: black;
+        color: white;
+        font-weight: bold;
+        cursor: pointer;
+        :hover {
+            background-color: lightgray;
+            color: black;
+        }
+    }
+`
+
+export const ButtonReturn = styled.div`
+    padding: 10px;
+    margin-top: 10px;
+    background-color: black;
+    color: white;
+    border: 1px solid white;
+    cursor: pointer;
+
+    :hover {
+    background-color: lightgray;
+    color: black;
+    }
+`
