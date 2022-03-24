640d7a4cc7ef8ed73d8f852ed18ba788027de7d6
Gabriela <gabriela.hermenegildo@gmail.com>
2022-03-24 15:13:13 -0300

    feito exercicios

---
+++ modulo4/aprofundamento-typescript/src/exercicio-2.ts
@@ -0,0 +1,33 @@
+//Exercício 2
+
+//a) A entrada é um array de números e a saída um objeto com o maior númbero, menor e a média de todos os números.
+
+function obterEstatisticas(numeros: number[]): { maior: number, menor: number, media: number } {
+
+    const numerosOrdenados = numeros.sort(
+        (a, b) => a - b
+    )
+
+    let soma = 0
+
+    for (let num of numeros) {
+        soma += num
+    }
+
+    const estatisticas = {
+        maior: numerosOrdenados[numeros.length - 1],
+        menor: numerosOrdenados[0],
+        media: soma / numeros.length
+    }
+
+    return estatisticas
+};
+
+//b) numerosOrdenados: number[]; soma: number
+
+//c)
+
+type amostraDeDados = {
+    numeros: number[]
+    obterEstatisticas: (numeros: number) => {maior: number, menor: number, media: number}
+};
---
+++ modulo4/aprofundamento-typescript/src/exercicio-5.ts
@@ -0,0 +1,2 @@
+// Exercício 5.
+//a) Mudou o diretório de algumas propriedades e alguns números.
