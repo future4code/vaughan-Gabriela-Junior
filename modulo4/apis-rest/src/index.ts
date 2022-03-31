f07017a5f4e00067ed277f47ce2081fb17645374
Gabriela <gabriela.hermenegildo@gmail.com>
2022-03-31 12:09:13 -0300

    Feito exercicio 1 e 2

---
+++ modulo4/apis-rest/src/index.ts
@@ -0,0 +1,56 @@
+import express, {Express} from 'express'
+import cors from 'cors'
+import { AddressInfo } from "net";
+import { users, Type, User } from './data';
+
+const app: Express = express();
+
+app.use(express.json());
+app.use(cors());
+
+//Exercício 1.
+// app.get("/users", (req, res) => {
+//    let errorCode = 400
+
+//    try {
+//       res.status(200).send(users)
+
+
+//    } catch (error: any) {
+
+//       res.status(errorCode).send({message:error.message});
+
+//    }
+// });
+
+//a) Método GET.
+//b) "/users"
+
+//Exercício 2.
+app.get("/users", (req, res) => {
+   let errorCode = 400
+
+   try {
+      const type: Type = req.query.type as Type
+      const user: User[] | undefined = users.filter((user) => user.type.toLowerCase() === type.toLowerCase());
+      if (!user) {
+         errorCode = 404;
+         throw new Error ("Não foi encontrado um usuário.");
+      }
+      res.status(200).send(user);
+
+   } catch (error: any) {
+      res.status(errorCode).send({message: error.message})
+
+   }
+})
+
+//Deixar por último
+const server = app.listen(process.env.PORT || 3003, () => {
+    if (server) {
+       const address = server.address() as AddressInfo;
+       console.log(`Server is running in http://localhost: ${address.port}`);
+    } else {
+       console.error(`Failure upon starting server.`);
+    }
+});
