615d1c9c08b452f6d80d4233a99587ad8da6a820
Gabriela <gabriela.hermenegildo@gmail.com>
2022-04-01 13:58:05 -0300

    feito tipo e array de usuarios

---
+++ modulo4/projeto-sistema-bancario/src/data.ts
@@ -0,0 +1,22 @@
+type User = {
+    name: string,
+    cpf: string,
+    birthDate: string
+};
+
+const users: User[] = [{
+    name: "João",
+    cpf: "123.456.789-12",
+    birthDate: "12/03/1990"
+    },
+    {
+    name: "Joana",
+    cpf: "321.654.987-21",
+    birthDate: "09/12/1987"
+    },
+    {
+    name: "Joaquina",
+    cpf: "789.456.123-99",
+    birthDate: "09/12/1987"
+    }
+];
