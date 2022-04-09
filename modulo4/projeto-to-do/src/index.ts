1ae7af2488fc1e321bc3c9878538a5470ce2819c
Gabriela <gabriela.hermenegildo@gmail.com>
2022-04-09 08:54:02 -0300

    feito endpoint 6

--- modulo4/projeto-to-do/src/index.ts
+++ modulo4/projeto-to-do/src/index.ts
@@ -122,22 +122,22 @@ app.put("/user/edit/:id", async (req: Request, res: Response) => {
 const createTask = async (title: string, description: string, limitDate: Date, creatorUserId: string): Promise<any> => {
     await connection("ToDoTask")
         .insert({
-           id: Date.now().toString(),
-           title: title,
-           description: description,
-           limit_date: limitDate,
-           creator_id: creatorUserId 
+            id: Date.now().toString(),
+            title: title,
+            description: description,
+            limit_date: limitDate,
+            creator_id: creatorUserId
         });
 };
 
 app.post("/task", async (req: Request, res: Response) => {
     try {
-        const {title, description, limitDate, creatorUserId} = req.body
+        const { title, description, limitDate, creatorUserId } = req.body
         console.log(new Date(limitDate))
         const dateFormatted = new Date(limitDate);
 
         if (!title || !description || !limitDate || !creatorUserId) {
-            throw new Error ("Todos os campos devem ser preenchidos.")
+            throw new Error("Todos os campos devem ser preenchidos.")
         } else {
             await createTask(title, description, dateFormatted, creatorUserId)
             res.status(201).send("Tarefa criada com sucesso!")
@@ -146,7 +146,7 @@ app.post("/task", async (req: Request, res: Response) => {
     } catch (error: any) {
         switch (error.message) {
             case "Todos os campos devem ser preenchidos.":
-                res.status(404).send(error.message)
+                res.status(422).send(error.message)
             default:
                 res.status(500).send(error.message);
         }
@@ -160,21 +160,45 @@ app.get("/task/:id", async (req: Request, res: Response) => {
         const id = req.params.id
         const task = await connection("ToDoTask").join('ToDoUser', 'ToDoTask.creator_id', '=', 'ToDoUser.id').where({ 'ToDoTask.id': id });
 
-        const taskMap = task.map((task) => {
-            return {
-                taskId: task.id,
-                title: task.title,
-                description: task.description,
-                limitDate: task.limit_date.toLocaleDateString('pt-br'),
-                status: task.status,
-                creatorUserId: task.creator_id,
-                creatorUserNickname: task.nickname
-            }
-        });
+        if (task.length === 0) {
+            throw new Error("Tarefa não encontrada.")
+        } else {
+            const taskMap = task.map((task) => {
+                return {
+                    taskId: task.id,
+                    title: task.title,
+                    description: task.description,
+                    limitDate: task.limit_date.toLocaleDateString('pt-br'),
+                    status: task.status,
+                    creatorUserId: task.creator_id,
+                    creatorUserNickname: task.nickname
+                }
+            });
 
-        res.status(200).send(taskMap);
+            res.status(200).send(taskMap);
+        }
+
     } catch (error: any) {
-        res.status(500).send(error.message);
+        switch (error.message) {
+            case "Tarefa não encontrada.":
+                res.status(404).send(error.message)
+            default:
+                res.status(500).send(error.message);
+        }
+    }
+});
+
+
+// 6.
+
+app.get("/users", async (req: Request, res: Response): Promise<any> => {
+    try {
+        const allTasks  = await connection ("ToDoUser")
+
+        res.status(200).send(allTasks)
+
+    } catch (error: any) {
+
     }
 });
 
