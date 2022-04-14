4bd8fd1493bcd15efebc2f1b613e9c71e56be211
Gabriela <gabriela.hermenegildo@gmail.com>
2022-04-14 15:08:41 -0300

    feito endpoint de buscar compras

--- modulo4/projeto-labecommerce/src/index.ts
+++ modulo4/projeto-labecommerce/src/index.ts
@@ -3,6 +3,7 @@ import { closeConnection, createTables } from "./data/migrations";
 import { createProduct } from "./endpoints/createProduct";
 import { createPurchase } from "./endpoints/createPurchase";
 import { createUser } from "./endpoints/createUser";
+import { getUserPurchases } from "./endpoints/gerUserPurchases";
 import { getAllProducts } from "./endpoints/getAllProducts";
 import { getAllUsers } from "./endpoints/getAllUsers";
 
@@ -15,3 +16,5 @@ app.post("/products", createProduct)
 app.get("/products", getAllProducts)
 
 app.post("/purchases", createPurchase)
+
+app.get("/users/:userId/purchases", getUserPurchases)
